727b04e8d9e4742bd22a2db719c9edaadf29b67d
josephLSalgado <josephLSalgado@outlook.com>
2020-12-08 21:43:11 -0600

    Agregado componente About e implementado en App

---
+++ src/components/About.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const About = () => (
+    <div className="About">
+        <div className="About-container">
+            <div className="About-avatar">
+                <figure>
+                    <img src="/" alt="">
+                    </img>
+                </figure>
+            </div>
+            <div className="About-name">
+                <h2>Joseph Salgado</h2>
+            </div>
+            <div className="About-profession">
+                <p>Blogger</p>
+                <p>Desarrollador de videjuegos</p>
+            </div>
+            <div className="About-desc">
+                <p>Soy desarrollador de videojuegos y me gusta la pizza. Actualmente estoy jugando Fortnite y Genshin Impact.</p>
+            </div>
+            <div className="About-location">
+                <p>San Luis Potosí, México.</p>
+            </div>
+            <div className="About-social">
+                Social
+            </div>
+        </div>
+    </div>
+);
+
+export default About;
---
+++ src/containers/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import About from '../components/About';
+import Main from '../components/Main';
+
+const App = () => {
+    return(
+        <Main>
+            <About />
+        </Main>
+    );
+}
+
+export default App;
